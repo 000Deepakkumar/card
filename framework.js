4fa30c1b08f6686514198cb2453dba232e3d9a7b
Nitish Kumar <118277328+000Nitish@users.noreply.github.com>
2024-04-28 17:21:11 +0530

    Add files via upload

---
+++ framework.js
@@ -0,0 +1,9 @@
+const name = document.getElementsByClassName("name");
+const file = document.getElementsByClassName("file");
+const mail = document.getElementsByClassName("mail");
+const number = document.getElementsByClassName("number");
+const job = document.getElementsByClassName("job");
+const btn = document.getElementById("btn");
+btn.onclick = () =>{
+    location.href="card.html";
+}
